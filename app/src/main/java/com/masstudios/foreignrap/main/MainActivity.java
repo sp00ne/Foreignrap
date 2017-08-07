ae245e5a8fe84fbb105df8b19fb802da9bbfb6cb
Mohsen Farzone <mohsen.farzone@netlight.com>
2017-08-07 20:16:18 +0200

    Add main activity; Add skeleton main layout

---
+++ app/src/main/java/com/masstudios/foreignrap/main/MainActivity.java
@@ -0,0 +1,13 @@
+package com.masstudios.foreignrap.main;
+
+import android.support.v4.app.ActivityCompat;
+
+/**
+ * The starting activity for the entire application
+ *
+ * @author mofa
+ * @since 2017-08-07
+ */
+
+public class MainActivity extends ActivityCompat {
+}
