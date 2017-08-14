f8ef3423906fb1265dc3ba6fb152d6449ecc397c
Mohsen Farzone <mohsen.farzone@netlight.com>
2017-08-14 14:45:12 +0200

    Made application dagger module

---
+++ app/src/main/java/com/masstudios/foreignrap/dagger/modules/ApplicationModule.java
@@ -0,0 +1,30 @@
+package com.masstudios.foreignrap.dagger.modules;
+
+import javax.inject.Singleton;
+
+import android.app.Application;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * The application dagger module, used for instance for caching
+ *
+ * @author mofa
+ * @since 2017-08-14
+ */
+
+@Module
+public class ApplicationModule {
+	Application mApplication;
+
+	public ApplicationModule(Application application) {
+		mApplication = application;
+	}
+
+	@Singleton
+	@Provides
+	Application providesApplication() {
+		return mApplication;
+	}
+}
